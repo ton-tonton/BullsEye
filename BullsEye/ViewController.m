dfbabcf1eecbea61f44b361158e0acc761f5c8db
Ton <phantom.ness@windowslive.com>
2015-04-22 11:06:06 +0700

    outlet properties

--- BullsEye/ViewController.m
+++ BullsEye/ViewController.m
@@ -10,7 +10,13 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *targetLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *roundLabel;
+@property (weak, nonatomic) IBOutlet UISlider* slider;
+
 @property (nonatomic) int currentValue;
+@property (nonatomic) int targetValue;
 
 @end
 
@@ -19,7 +25,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.currentValue = 50;
+    [self startNewRound];
+    [self updateLabels];
 }
 
 - (void)didReceiveMemoryWarning
@@ -28,20 +35,38 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - action
+
 - (IBAction)showAlert
 {
     NSString *message = [NSString stringWithFormat:@"The value of slider is %d", self.currentValue];
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Hello wolrd"
                                                         message:message
                                                        delegate:nil
-                                              cancelButtonTitle:@"Awesome"
+                                              cancelButtonTitle:@"OK"
                                               otherButtonTitles:nil];
     [alertView show];
+    [self startNewRound];
+    [self updateLabels];
 }
 
 - (IBAction)sliderMoved:(UISlider *)slider
 {
     self.currentValue = roundf(slider.value);
+}
+
+#pragma mark - function
+
+- (void)startNewRound
+{
+    self.currentValue = 50;
+    self.slider.value = self.currentValue;
+    self.targetValue = 1 + arc4random_uniform(100);
+}
+
+- (void)updateLabels
+{
+    self.targetLabel.text = [NSString stringWithFormat:@"%d", self.targetValue];
 }
 
 @end
