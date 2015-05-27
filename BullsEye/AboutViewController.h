54175377444aaa7c00df3180e3439611db0c787f
Ton <phantom.ness@windowslive.com>
2015-05-27 13:38:38 +0700

    add AboutViewController

---
+++ BullsEye/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Tawatchai Sunarat on 5/27/15.
+//  Copyright (c) 2015 ton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ BullsEye/AboutViewController.m
@@ -0,0 +1,35 @@
+//
+//  AboutViewController.m
+//  BullsEye
+//
+//  Created by Tawatchai Sunarat on 5/27/15.
+//  Copyright (c) 2015 ton. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - action
+
+- (IBAction)close:(id)sender
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
