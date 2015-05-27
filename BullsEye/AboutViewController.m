9c48e2dae98430be7e2e066b6a196674959b309a
Ton <phantom.ness@windowslive.com>
2015-05-27 15:45:07 +0700

    add UIWebView to AboutViewController

--- BullsEye/AboutViewController.m
+++ BullsEye/AboutViewController.m
@@ -10,18 +10,26 @@
 
 @interface AboutViewController ()
 
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
 @end
 
 @implementation AboutViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"BullsEye" ofType:@"html"];
+    NSData *htmlData = [NSData dataWithContentsOfFile:htmlFile];
+    NSURL *baseUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]];
+    [self.webView loadData:htmlData
+                  MIMEType:@"text/html"
+          textEncodingName:@"UTF-8"
+                   baseURL:baseUrl];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (BOOL)prefersStatusBarHidden
